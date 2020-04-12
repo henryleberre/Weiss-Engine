6f0dfd1f2b753dee4667c62fbe1b22eaa70defe7
PolarToCartesian <hleberre64@gmail.com>
2020-04-12 18:55:37 +0200

    Better Buffer Management (pt1)

--- graphics/Graphics.cpp
+++ graphics/Graphics.cpp
@@ -1,11 +1,1 @@
 #include "Graphics.h"
-
-GraphicsEngine::GraphicsEngine(const RenderAPIName& apiName)
-{
-	this->m_pRenderAPI = RenderAPI::Create(apiName);
-}
-
-GraphicsEngine::~GraphicsEngine()
-{
-
-}
