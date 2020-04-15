1691d9b1bf811264768f8c80ab5d87359641bc62
PolarToCartesian <hleberre64@gmail.com>
2020-04-16 00:06:13 +0200

    Added Image Encoding & More Image Constructors

--- image/windows/Image.h
+++ image/windows/Image.h
@@ -6,7 +6,13 @@
 
 class WindowsImage : public ImageBase {
 public:
+    WindowsImage();
+    WindowsImage(WindowsImage&& other);
+    WindowsImage(const WindowsImage& other);
     WindowsImage(const char* filename);
+    WindowsImage(const uint16_t width, const uint16_t height, const Coloru8& fillColor = { 255u,255u,255u,255u });
+
+    virtual void Write(const char* filename) const override;
 };
 
 #endif // __WEISS__OS_WINDOWS
