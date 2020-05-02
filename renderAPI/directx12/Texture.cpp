23efaec8dc65c15127ad6ec651f1f4c9800db51e
PolarToCartesian <hleberre64@gmail.com>
2020-05-02 22:32:45 +0200

    dx12 Textures Working!

--- renderAPI/directx12/Texture.cpp
+++ renderAPI/directx12/Texture.cpp
@@ -17,7 +17,7 @@ namespace D3D12    {
 		resourceDesc.Height = image.GetHeight();
 		resourceDesc.DepthOrArraySize = 1;
 		resourceDesc.MipLevels = useMipmaps ? 0 : 1;
-		resourceDesc.Format = DXGI_FORMAT::DXGI_FORMAT_R8G8B8A8_UINT;
+		resourceDesc.Format = DXGI_FORMAT::DXGI_FORMAT_R8G8B8A8_UNORM;
 		resourceDesc.SampleDesc.Count   = 1;
 		resourceDesc.SampleDesc.Quality = 0;
 		resourceDesc.Layout = D3D12_TEXTURE_LAYOUT_UNKNOWN;
@@ -37,7 +37,7 @@ namespace D3D12    {
 		this->m_textureResourceView.Shader4ComponentMapping = D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING;
 		this->m_textureResourceView.Format = resourceDesc.Format;
 		this->m_textureResourceView.ViewDimension = D3D12_SRV_DIMENSION_TEXTURE2D;
-		this->m_textureResourceView.Texture2D.MipLevels = 1;
+		this->m_textureResourceView.Texture2D.MipLevels = useMipmaps ? 0 : 1;
 		
 		pDevice->CreateShaderResourceView(this->m_textureResource, &this->m_textureResourceView, this->m_descriptorHeap->GetCPUDescriptorHandleForHeapStart());
 
@@ -47,11 +47,6 @@ namespace D3D12    {
 	D3D12DescriptorHeap& D3D12Texture::GetDescriptorHeap()
 	{
 		return this->m_descriptorHeap;
-	}
-
-	void D3D12Texture::Bind()
-	{
-		(*this->m_pCommandList)->SetGraphicsRootDescriptorTable(1u, this->m_descriptorHeap->GetGPUDescriptorHandleForHeapStart());
 	}
 
 	size_t D3D12Texture::GetSlot() const noexcept
