b943007aacd3e2234f135f8b2647e30c02b79297
PolarToCartesian <24208000+henryleberre@users.noreply.github.com>
2020-05-14 19:23:42 +0200

    Changed Include PATH

--- win_cl.bat
+++ win_cl.bat
@@ -4,7 +4,7 @@ mkdir bin
 :: - the "LIBPATH" option
 :: - The "/I" options
 cl Source.cpp /I "C:\VulkanSDK\Include" ^
-              /I "C:\Code\3D Engines\Realtime\Weiss\d3dx12" ^
+              /I "C:\Code\3D Engines\Realtime\Weiss\Weiss\d3dx12" ^
               /fp:fast /O2 /Ot /Ob3 /EHsc /std:c++17 ^
               /link /LIBPATH:%2 /OUT:bin/%1
 
