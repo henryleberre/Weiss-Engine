e78a6aae68c032eb939cdced27c0ff0c4d837198
PolarToCartesian <24208000+henryleberre@users.noreply.github.com>
2020-05-14 21:32:01 +0200

    Added Missing Functions

--- Weiss.hpp
+++ Weiss.hpp
@@ -1030,6 +1030,12 @@ namespace WS
 		void CalculateTransform() noexcept;
 	};
 
+	// ////////////////-\\\\\\\\\\\\\\\\ \\
+	// [/‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾\| \\
+	// |----------Conversions----------| \\
+	// |\_____________________________/| \\
+	// \\\\\\\\\\\\\\\\-//////////////// \\
+
 	[[nodiscard]] inline float DegreesToRadians(const float degrees);
 	[[nodiscard]] inline float RadiansToDegrees(const float radians);
 
@@ -3791,6 +3797,73 @@ namespace WS {
 				mat[i][i] = 1.0f;
 
 		return mat;
+	}
+
+	// ///////////////-\\\\\\\\\\\\\\\ \\
+	// [/‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾\| \\
+	// |----------Transform----------| \\
+	// |\___________________________/| \\
+	// \\\\\\\\\\\\\\\-/////////////// \\
+
+	void Transform::CalculateTransform()
+	{
+#ifdef __WEISS__OS_WINDOWS
+
+		this->m_transform = DirectX::XMMatrixTranslationFromVector(this->m_translation)
+						  * DirectX::XMMatrixRotationX(this->m_rotation.x)
+						  * DirectX::XMMatrixRotationY(this->m_rotation.y)
+						  * DirectX::XMMatrixRotationZ(this->m_rotation.z);
+
+#endif // __WEISS__OS_WINDOWS
+	}
+
+	// ////////////////-\\\\\\\\\\\\\\\\ \\
+	// [/‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾\| \\
+	// |----------Conversions----------| \\
+	// |\_____________________________/| \\
+	// \\\\\\\\\\\\\\\\-//////////////// \\
+
+	[[nodiscard]] inline float DegreesToRadians(const float degrees)
+	{
+		return degrees * PI / 180.0f;
+	}
+
+	[[nodiscard]] inline float RadiansToDegrees(const float radians)
+	{
+		return radians / PI * 180.0f;
+	}
+
+	// If a polar point is stored as (θ, r)
+	[[nodiscard]] inline Vec2f PolarToCartesian(const Vec2f polar) // :)
+	{
+		const float x = polar.y * std::cos(polar.x);
+		const float y = polar.y * std::sin(polar.x);
+
+		return Vec2f{ x, y };
+	}
+
+	[[nodiscard]] inline Vec2f CartesianToPolar(const Vec2f cartesian)
+	{
+		float a = std::atan(cartesian.y / cartesian.x);
+
+		if (cartesian.x < 0) a += PI;
+		else if (cartesian.y < 0) a += TWO_PI;
+
+		const float r = std::sqrt(std::pow(cartesian.x, 2) + std::pow(cartesian.y, 2));
+
+		return Vec2f { a, r };
+	}
+
+	[[nodiscard]] inline Vec3f GetTriangleSurfaceNormal(const Vec3f& a, const Vec3f& b, const Vec3f& c)
+	{
+		const Vec3f U = b - a;
+		const Vec3f V = c - a;
+
+		return Vec3f {
+			U.y * V.z - U.z * V.y,
+			U.z * V.x - U.x * V.z,
+			U.x * V.y - U.y * V.x
+		};
 	}
 
 	/*
