e9015bd686b40dd6fa957edf13d5150940f6277b
PolarToCartesian <24208000+henryleberre@users.noreply.github.com>
2020-05-15 21:20:23 +0200

    Added scripts/

--- scripts/win32.bat
+++ scripts/win32.bat
@@ -1,6 +1,8 @@
 :: Open the "x86 Native Tools Command Prompt for VS 2019"
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars32.bat"
 
+cd ../
+
 :: Compile
 call "win_cl.bat" win32.exe "C:\VulkanSDK\Lib32"
 
--- scripts/win64.bat
+++ scripts/win64.bat
@@ -1,6 +1,8 @@
 :: Open the "x64 Native Tools Command Prompt for VS 2019"
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 
+cd ../
+
 :: Compile
 call "win_cl.bat" win64.exe "C:\VulkanSDK\Lib"
 
